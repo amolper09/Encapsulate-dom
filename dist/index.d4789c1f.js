9d137ba71e133773e6df0c80f3e0a6bad0e19f0c
Gary <guorui11996@163.com>
2022-10-10 15:50:15 +0800

    'version1'

---
+++ dist/index.d4789c1f.js
@@ -0,0 +1,33 @@
+// const div = dom.create("<div>3</div>");
+// console.log(div);
+// let div2 = dom.remove(test);
+// console.log(div2);
+// dom.empty(empty);
+// dom.attr(test, "title", "this is a new title");
+// console.log(test);
+// const title = dom.attr(test, "title");
+// console.log(title);
+// dom.text(test,'newnew')
+// dom.style(test, { border: "1px solid red", background: "blue" });
+// dom.style(test, "border");
+// dom.style(test, "border", "1px solid black");
+// console.log(test);
+// console.log(dom.style(test, "border"));
+// const fn = () => {
+//   console.log("点击了");
+// };
+// dom.on(test, "click", fn);
+// dom.off(test, "click", fn);
+// const div2 = dom.find("#test")[0];
+// console.log(dom.children(empty));
+// console.log(dom.siblings(e2));
+// console.log(dom.next(e2));
+// console.log(dom.previous(e2));
+const div = dom.find("#test>.red")[0];
+console.log(div);
+dom.style(div, "color", "red");
+const divList = dom.find(".red");
+console.log(divList);
+dom.each(divList, (n)=>console.log(n));
+
+//# sourceMappingURL=index.d4789c1f.js.map
---
+++ src/dom.js
@@ -0,0 +1,100 @@
+window.dom = {
+  create(string) {
+    const container = document.createElement("template");
+    container.innerHTML = string.trim();
+    return container.content.firstChild;
+  },
+  //增
+  after(node, newNode) {
+    node.parentNode.insertBefore(newNode, node.nextSibling);
+  },
+  before(newNode, node) {
+    node.parentNode.insertBefore(newNode, node);
+  },
+  append(parent, child) {
+    parent.appendChild(child);
+  },
+  wrap(child, father) {
+    dom.before(father, child);
+    dom.append(father, child);
+  },
+  //删
+  remove(child) {
+    child.parentNode.removeChild(child);
+    return child;
+  },
+  empty(child) {
+    const array = [];
+    let x = child.firstChild;
+    while (x) {
+      array.push(dom.remove(child.firstChild));
+      // console.log(array);
+      x = child.firstChild;
+    }
+    return array;
+  },
+  //改
+  attr(node, name, value) {
+    if (arguments.length === 3) {
+      node.setAttribute(name, value);
+    }
+    if (arguments.length === 2) {
+      return node.getAttribute(name);
+    }
+  },
+  text(node, string) {
+    node.innerHTML = string;
+  },
+  style(node, name, value) {
+    if (arguments.length === 3) {
+      node.style[name] = value;
+    } else if (arguments.length === 2) {
+      if (typeof name === "string") {
+        return node.style[name];
+      } else if (name instanceof Object) {
+        const object = name;
+        for (let key in object) {
+          node.style[key] = object[key];
+        }
+      }
+    }
+  },
+  on(node, eventName, fn) {
+    node, addEventListener(eventName, fn);
+  },
+  off(node, eventName, fn) {
+    node, removeEventListener(eventName, fn);
+  },
+  //查
+  find(selector, scope) {
+    return (scope || document).querySelectorAll(selector);
+  },
+  parent(node) {
+    return node.parentNode;
+  },
+  children(node) {
+    return node.children;
+  },
+  siblings(node) {
+    return Array.from(node.parentNode.children).filter((n) => n !== node);
+  },
+  next(node) {
+    let x = node.nextSibling;
+    while (x && x.nodeType === 3) {
+      x = x.nextSibling;
+    }
+    return x;
+  },
+  previous(node) {
+    let x = node.previousSibling;
+    while (x && x.nodeType === 3) {
+      x = x.previousSibling;
+    }
+    return x;
+  },
+  each(nodeList, fn) {
+    for (let i = 0; i < nodeList.length; i++) {
+      fn.call(null, nodeList[i]);
+    }
+  },
+};
